fda837edc8b4fac0c0bda2610555707d899331bf
Pradeep Bhattarai <29733866+pr0d33p@users.noreply.github.com>
2019-11-18 09:24:46 +0545

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication{
+	public static void main(String[] args) {
+		int num1, num2, result;
+		num1 = 12;
+		num2 = 5;
+		result = num1*num2;
+		System.out.println(result);
+	}
+}
