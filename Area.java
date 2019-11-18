f5f8701a30984a174e136ac9faef1cb871f45d7a
Pradeep Bhattarai <29733866+pr0d33p@users.noreply.github.com>
2019-11-18 09:23:05 +0545

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,17 @@
+// Write a program to calculate the area and perimeter of rectangle(where length=20 and breadth=30)
+
+// Write a program to calculate the simple interest where (principle=100,time=10,rate=2)
+
+// write a program to find the speed of a car covered 10 km in 1 hour
+
+// Write a program to find the perimeter of circle
+
+// Write a program to calculate the celsius(36 degree) into fahrenheit
+
+
+
+public class AreaParimeter {
+	public static void main(String[] args) {
+		
+	}
+}
